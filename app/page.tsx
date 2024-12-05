152f14abd5a2ffea002475e81126d61977b3284f
Parsa <149250117+parsa-vesali@users.noreply.github.com>
2024-12-05 20:03:33 +0330

    the project updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PassKeyModal from "@/components/PassKeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({searchParams} : SearchParamProps) {
+
+  const isAdmin = searchParams.admin === 'true'
+  
+
   return (
     <div className="flex h-screen max-h-screen">
       {/* OTP */}
+      {isAdmin && <PassKeyModal />}
       <section className="remove-scrollbar container">
         <div className="sub-container max-w-[496px]">
           <span className="flex items-center gap-2 mb-8">
