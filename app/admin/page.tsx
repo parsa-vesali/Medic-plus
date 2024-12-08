45465198587626405aaf014eb4f766834c397399
Parsa <149250117+parsa-vesali@users.noreply.github.com>
2024-12-08 21:26:22 +0330

    the project updated

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,32 +1,60 @@
 import StatCard from '@/components/StatCard'
-import {columns, Payment} from '@/components/table/Columns';
-import {DataTable} from '@/components/table/DataTable';
+import { columns, } from '@/components/table/Columns';
+import { DataTable } from '@/components/table/DataTable';
+import { Input } from '@/components/ui/input';
 import { getRecentAppointmentList } from '@/lib/actions/appointment.actions';
 import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
+import { HiOutlineArrowLeftStartOnRectangle, HiOutlineBell, HiOutlineUser } from "react-icons/hi2";
+import { ImCommand } from "react-icons/im";
 
 
 const Admin = async () => {
     const appointments = await getRecentAppointmentList();
 
     return (
-        <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+        <div className='mx-auto flex max-w-7xl flex-col space-y-14 '>
             <header className='admin-header'>
-                <span className="flex items-center gap-2">
+                <Link href='/' className="flex items-center gap-1">
                     <Image
                         src='/assets/icons/logo-icon.svg'
                         height={1000}
                         width={1000}
                         alt="Logo"
-                        className="w-fit"
+                        className="w-8"
                     />
                     <h2 className="font-Dana-Bold text-xl"> مدیک‌<span className=" text-sky-500">پلاس</span></h2>
-                </span>
-                <p className='text-2xl font-Dana-Medium'>داشبورد ادمین</p>
+                </Link>
 
+                <button className='relative flex h-10 px-3 bg-dark-200 items-center justify-center rounded-xl cursor-pointer'>
+                    <ImCommand className='w-5 h-5 text-dark-600' />
+                    <Input
+                        type='search'
+                        className='search-input'
+                        placeholder='جستجو ...' />
+                </button>
+
+                <div className="flex items-center gap-x-3">
+                    <span className='relative w-10 h-10 flex items-center justify-center bg-dark-200 rounded-xl cursor-pointer'>
+                        <HiOutlineBell className='w-6 h-6 text-zinc-200' />
+                        <span className="absolute top-0 left-0 flex h-2 w-2">
+                            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                            <span className="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
+                        </span>
+                    </span>
+                    <span className='relative w-10 h-10 flex items-center justify-center bg-dark-200 rounded-xl cursor-pointer'>
+                        <HiOutlineUser className='w-6 h-6 text-zinc-200' />
+                    </span>
+                    <Link href='/' className='relative px-3 h-10 flex gap-x-1 items-center justify-center bg-dark-200 rounded-xl cursor-pointer'>
+                        <HiOutlineArrowLeftStartOnRectangle className='w-6 h-6 text-zinc-200' />
+                        <p className='text-sm'>خروج</p>
+                    </Link>
+                </div>
             </header>
 
-            <main className='admin-main'>
+
+            {/* <main className='admin-main'>
                 <section className='w-full space-y-4'>
                     <h2 className='header'>خوش آمدید 👋</h2>
                     <p className='text-dark-700'>
@@ -56,7 +84,7 @@ const Admin = async () => {
                 </section>
 
                 <DataTable columns={columns} data={appointments.documents} />
-            </main>
+            </main> */}
         </div>
     )
 }
